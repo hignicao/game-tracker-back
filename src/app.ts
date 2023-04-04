145cba1d04dbf05053965cf4a35ff278dd82e921
Hugo Ignacio <hugo.ignacio1998@gmail.com>
2023-04-04 04:19:24 -0300

    feat: base files, back-end now running

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import "express-async-errors";
+import express, { json, Request, Response, Express } from "express";
+import cors from "cors";
+import { connectDb, disconnectDB } from "./config";
+
+const app = express();
+app
+	.use(json())
+	.use(cors())
+	.get("/health", (req: Request, res: Response) => res.send("I'am alive!"));
+
+export function init(): Promise<Express> {
+	connectDb();
+	return Promise.resolve(app);
+}
+
+export async function close(): Promise<void> {
+	await disconnectDB();
+}
+export default app;

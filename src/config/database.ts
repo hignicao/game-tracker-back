3fe8e221291dd70ec3952ff5269fd3c4796a8017
Hugo Ignacio <hugo.ignacio1998@gmail.com>
2023-04-18 04:00:44 -0300

    feat: added redis url

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,14 +2,16 @@ import { PrismaClient } from "@prisma/client";
 import { createClient } from "redis";
 
 export let prisma: PrismaClient;
-export const redisClient = createClient();
+export const redisClient = createClient({
+	url: process.env.REDIS_URL,
+});
 
 export async function connectDb(): Promise<void> {
-  prisma = new PrismaClient();
-  await redisClient.connect();
+	prisma = new PrismaClient();
+	await redisClient.connect();
 }
 
 export async function disconnectDB(): Promise<void> {
-  await prisma?.$disconnect();
-  await redisClient.quit();
+	await prisma?.$disconnect();
+	await redisClient.quit();
 }
